1576cbb229d09b840ebab0451d45db9e793f1c7c
maxxx80 <maxjvc11+1@gmail.com>
2021-05-30 06:45:20 +0300

    seq logger

--- dapr/fsharp/shared/logging.fs
+++ dapr/fsharp/shared/logging.fs
@@ -9,17 +9,20 @@ module Logging =
     open System
     open Serilog
     open Serilog.Sinks.Elasticsearch
+    open Serilog.Sinks.Seq
     open Serilog.Enrichers.Span
     
     let createSerilogLogger (configuration: IConfiguration) (webHostBuilder: IWebHostBuilder) =
         let elasticHost = configuration.Item("ElasticHost")
-        
-        let logger = 
+        let seqHost = configuration.Item("SeqHost")        
+        let mutable logger = 
             LoggerConfiguration()
                 .ReadFrom.Configuration(configuration)
                 .Enrich.FromLogContext()
                 .Enrich.WithSpan()
-                .WriteTo.Elasticsearch(ElasticsearchSinkOptions(Uri elasticHost))
-                .CreateLogger()
-        Log.Logger <- logger                    
+        if String.IsNullOrEmpty(elasticHost) |> not then
+            logger <- logger.WriteTo.Elasticsearch(ElasticsearchSinkOptions(Uri elasticHost))
+        if String.IsNullOrEmpty(seqHost) |> not then
+            logger <- logger.WriteTo.Seq(seqHost)
+        Log.Logger <- logger.CreateLogger()
         webHostBuilder.UseSerilog()
