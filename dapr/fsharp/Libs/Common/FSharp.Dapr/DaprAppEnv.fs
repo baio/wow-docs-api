19f7bbf11d54a2a05d52d19feb8443058451359e
maxxx80 <maxjvc11+1@gmail.com>
2022-09-15 11:01:20 +0300

    cleanup

--- dapr/fsharp/Libs/Common/FSharp.Dapr/DaprAppEnv.fs
+++ dapr/fsharp/Libs/Common/FSharp.Dapr/DaprAppEnv.fs
@@ -3,15 +3,4 @@
 open Dapr.Client
 open Microsoft.Extensions.Logging
 
-(*
-type IDaprAppEnv =
-    abstract member GetLogger : unit -> ILogger
-    abstract member GetDapr : unit -> DaprClient
-*)
-
 type DaprAppEnv = { Logger: ILogger; Dapr: DaprClient }
-(*
-    interface IDaprAppEnv with
-        member __.GetLogger() = __.Logger
-        member __.GetDapr() = __.Dapr
-*)
